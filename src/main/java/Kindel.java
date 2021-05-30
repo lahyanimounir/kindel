a69d5beafe74e6bb3e17b06c841969fce4afce47
Ayoub beinagri <baddou.ayoub96@gmail.com>
2021-05-30 19:39:30 +0200

    30-05-2021

---
+++ src/main/java/Kindel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author rachad
+ */
+public class Kindel {
+    
+    public static void main(String[] args){
+        
+        // afficher le menu
+        //consultations
+    }
+    
+}
